dd0232125bc536c868b17724f289bd8ed1cb23b6
Subhodeep Das <subhodeepdas777@gmail.com>
2020-08-22 09:15:23 +0530

    Add files via upload

---
+++ Engineerbabushop/Shop/README.md
@@ -0,0 +1,10 @@
+### Package installation
+```bash
+npm install
+```
+ ### Start the React App
+ Excute the following command: 
+```bash
+npm start
+```
+The application will start automatically in your browser on http://localhost:3000
---
+++ Engineerbabushop/Shop/src/components/reducers/cartReducer.js
@@ -0,0 +1,117 @@
+import Item1 from '../../images/item1.jpg'
+import Item2 from '../../images/item2.jpg'
+import Item3 from '../../images/item3.jpg'
+import Item4 from '../../images/item4.jpg'
+import Item5 from '../../images/item5.jpg'
+import Item6 from '../../images/item6.jpg'
+import { ADD_TO_CART,REMOVE_ITEM,SUB_QUANTITY,ADD_QUANTITY,ADD_SHIPPING } from '../actions/action-types/cart-actions'
+
+
+const initState = {
+    items: [
+        {id:1,title:'Reebok', desc: "Best shoes from Reebok. Avaliable at exculsive price.", price:5499,img:Item1},
+        {id:2,title:'Adidas', desc: "New shoes from Adidas. Avaliable at all time low price.", price:4999,img: Item2},
+        {id:3,title:'Vans', desc: "Top shoes from Vans. Avaliable at exculsive price.",price:2699,img: Item3},
+        {id:4,title:'Nick', desc: "Best shoes from The Nick. Avaliable at all time low price.", price:3599,img:Item4},
+        {id:5,title:'Bata', desc: "New shoes from Bata. Avaliable at exculsive price.", price:2199,img: Item5},
+        {id:6,title:'Puma', desc: "Top shoes from Puma. Avaliable at all time low price.", price:1199,img: Item6},
+    ],
+    addedItems:[],
+    total: 0
+
+}
+const cartReducer= (state = initState,action)=>{
+   
+    //INSIDE HOME COMPONENT
+    if(action.type === ADD_TO_CART){
+          let addedItem = state.items.find(item=> item.id === action.id)
+          //check if the action id exists in the addedItems
+         let existed_item= state.addedItems.find(item=> action.id === item.id)
+         if(existed_item)
+         {
+            addedItem.quantity += 1 
+             return{
+                ...state,
+                 total: state.total + addedItem.price 
+                  }
+        }
+         else{
+            addedItem.quantity = 1;
+            //calculating the total
+            let newTotal = state.total + addedItem.price 
+            
+            return{
+                ...state,
+                addedItems: [...state.addedItems, addedItem],
+                total : newTotal
+            }
+            
+        }
+    }
+    if(action.type === REMOVE_ITEM){
+        let itemToRemove= state.addedItems.find(item=> action.id === item.id)
+        let new_items = state.addedItems.filter(item=> action.id !== item.id)
+        
+        //calculating the total
+        let newTotal = state.total - (itemToRemove.price * itemToRemove.quantity )
+        console.log(itemToRemove)
+        return{
+            ...state,
+            addedItems: new_items,
+            total: newTotal
+        }
+    }
+    //INSIDE CART COMPONENT
+    if(action.type=== ADD_QUANTITY){
+        let addedItem = state.items.find(item=> item.id === action.id)
+          addedItem.quantity += 1 
+          let newTotal = state.total + addedItem.price
+          return{
+              ...state,
+              total: newTotal
+          }
+    }
+    if(action.type=== SUB_QUANTITY){  
+        let addedItem = state.items.find(item=> item.id === action.id) 
+        //if the qt == 0 then it should be removed
+        if(addedItem.quantity === 1){
+            let new_items = state.addedItems.filter(item=>item.id !== action.id)
+            let newTotal = state.total - addedItem.price
+            return{
+                ...state,
+                addedItems: new_items,
+                total: newTotal
+            }
+        }
+        else {
+            addedItem.quantity -= 1
+            let newTotal = state.total - addedItem.price
+            return{
+                ...state,
+                total: newTotal
+            }
+        }
+        
+    }
+
+    if(action.type=== ADD_SHIPPING){
+          return{
+              ...state,
+              total: state.total + 120
+          }
+    }
+
+    if(action.type=== 'SUB_SHIPPING'){
+        return{
+            ...state,
+            total: state.total - 120
+        }
+  }
+    
+  else{
+    return state
+    }
+    
+}
+
+export default cartReducer
